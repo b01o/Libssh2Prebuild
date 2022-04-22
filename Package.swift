7f59ce903bd8883f3d566b59b9de5c62ad9ea8f3
Jonathan Huang <expine@163.com>
2022-04-22 10:07:07 +0800

    Build 1.10.0

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "CSSH",
-                      url: "https://github.com/DimaRU/Libssh2Prebuild/releases/download/1.10.0+OpenSSL_1_1_1l/CSSH-1.10.0+OpenSSL_1_1_1l.xcframework.zip",
-                      checksum: "a4aff717265efc34f494618a489fdcaec7f50cf770343449e72113cf794c0ca5")
+                      url: "https://github.com/b01o/Libssh2Prebuild/releases/download/1.10.0/CSSH-1.10.0+OpenSSL_1_1_1l.xcframework.zip",
+                      checksum: "bd2eb2b37618cf17e3f47831c35f82a379672347b349d4df65108df4cd303105")
     ]
 )
